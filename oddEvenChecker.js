e5ae3145aeeba2cced142c8ba9ab26ed503b9e75
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-25 23:14:37 +0600

    odd even checker programme complete

---
+++ oddEvenChecker.js
@@ -0,0 +1,21 @@
+// Problem: EvenOdd Checker & Sum of a Range
+
+// without function
+// let number = 20;
+
+// if (number % 2 === 0) {
+//   console.log("Even Number");
+// } else {
+//   console.log("Odd Number");
+// }
+
+// with funtion
+function oddEvenChecker(number) {
+  if (number % 2 === 0) {
+    return "Even Number";
+  } else {
+    return "Odd Number";
+  }
+}
+console.log(oddEvenChecker(23));
+console.log(oddEvenChecker(50));
