859126e17a5ee328a429fa0a16088468f6c81583
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-25 23:30:04 +0600

    sum of range programme complete

---
+++ sumOfRange.js
@@ -0,0 +1,20 @@
+// Problem:sum of a range of a number (1 to N)
+
+// without function
+// let number = 7;
+// let sum = 0;
+// for (let i = 1; i <= number; i++) {
+//   sum = sum + i;
+// }
+// console.log("sum:", sum);
+
+// with function
+function sumOfRange(number) {
+  sum = 0;
+  for (let i = 1; i <= number; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+const result = sumOfRange(7);
+console.log("sum:", result);
