7fd49d8c6c58d22141536c08733d5bc71a3d6e3b
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-27 17:51:00 +0600

    modified

---
+++ productOfDigits.js
@@ -0,0 +1,23 @@
+/* Problem Statement: 
+Write a function productOfDigits that takes a positive whole number and 
+returns the product of its individual digits. 
+Input Output 
+123 =  6  (1 × 2 × 3) 
+4040 =  0 */
+
+// Input: a number
+// Output: product of its digits
+// Returns: a number
+
+function productOfDigits(num) {
+  //   let str = num.toString();
+  let total = 1;
+  for (let i = 1; i < num.length; i++) {
+    total *= num[i];
+  }
+
+  return `Product of digits`;
+}
+
+console.log(productOfDigits(123)); // Expected: 6
+console.log(productOfDigits(4040)); // Expected: 0
