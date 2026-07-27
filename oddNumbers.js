90ec466b8561e3966c977027a2a510ddc17897ad
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-27 18:44:42 +0600

    get odd numbers up to N programme complete

---
+++ oddNumbers.js
@@ -0,0 +1,24 @@
+/* Odd Numbers Up To N 
+Problem Statement: 
+Write a function getOddNumbers that takes a number n and returns an array 
+of all odd numbers from 1 to n (inclusive). 
+Input 
+Output 
+10 
+[1, 3, 5, 7, 9]  */
+
+// Input: a number
+// Output: array of odd numbers from 1 to n
+// Returns: an array
+function getOddNumbers(n) {
+  let odds = [];
+
+  for (let i = 1; i < n; i++) {
+    if (i % 2 === 1) {
+      odds.push(i);
+    }
+  }
+  return odds;
+}
+console.log(getOddNumbers(10));
+// Expected: [1, 3, 5, 7, 9]
