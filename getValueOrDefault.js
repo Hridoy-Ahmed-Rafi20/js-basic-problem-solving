7fdff94b19e33839fd450d142208e640faf49834
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-28 11:56:46 +0600

    get value or default programme complete

---
+++ getValueOrDefault.js
@@ -0,0 +1,27 @@
+/* Task 13-8B: Get a Value or a Default 
+Problem Statement: 
+Write a function getValueOrDefault that takes an object, a key name, and a 
+default value, and returns the object's value for that key if it exists, otherwise 
+the default value. 
+Input Output 
+person, key "age", default 0 25 
+person, key "grade", default "N/A" "N/A" */
+
+// Input: an object, a key (string), a default value
+// Output: the value at that key, or the default
+// Returns: any value
+
+function getValueOrDefault(obj, key, defaultValue) {
+  //   for (let keys in obj) {
+
+  if (Object.hasOwn(obj, key)) {
+    return obj[key];
+  } else {
+    return defaultValue;
+  }
+  //   }
+}
+
+let person = { name: "Sam", age: 25 };
+console.log(getValueOrDefault(person, "age", 0)); // Expected: 25
+console.log(getValueOrDefault(person, "grade", "N/A")); // Expected: ("N/A");
